c2c81df73bb140b689cbe522071b68dca5ec0a95
AbdulRehman7269 <syedabdulrehman7269@gmail.com>
2022-11-28 13:49:46 +0500

    Practice in StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,72 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void printAnswer() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questionList = [
+      'Question 1',
+      'Question 2',
+      'Question 3',
+      'Question 4',
+    ];
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quix App'),
+        ),
+        body: Column(
+          children: [
+            Text(questionList[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: printAnswer,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Pressed Answer 2');
+              },
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Pressed Answer 3'),
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Pressed Answer 4'),
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -14,27 +80,26 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
   var questionIndex = 0;
-  void questionPrint() {
+  void printAnswer() {
     setState(() {
       questionIndex = questionIndex + 1;
     });
-
     print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questionLists = [
-      '1',
-      '2',
-      '3',
-      '4',
+      'Question 1',
+      'Question 2',
+      'Question 3',
+      'Question 4',
     ];
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My Quizz App'),
+          title: Text('MY Quix App'),
         ),
         body: Column(
           children: [
@@ -42,22 +107,22 @@ class MyAppState extends State<MyApp> {
               questionLists[questionIndex],
             ),
             ElevatedButton(
-              onPressed: questionPrint,
-              child: Text('Answer1'),
-            ),
-            ElevatedButton(
-              onPressed: () => print('2'),
-              child: Text('Answer2'),
+              onPressed: printAnswer,
+              child: Text('Answer 1'),
             ),
             ElevatedButton(
               onPressed: () {
-                print('3');
+                print('Pressed Answer 2');
               },
-              child: Text('Answer3'),
+              child: Text('Answer 2'),
             ),
             ElevatedButton(
-              onPressed: () => print('4'),
-              child: Text('Answer4'),
+              onPressed: () => print('Pressed Answer 3'),
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Pressed Answer 4'),
+              child: Text('Answer 4'),
             ),
           ],
         ),
@@ -65,3 +130,4 @@ class MyAppState extends State<MyApp> {
     );
   }
 }
+*/
